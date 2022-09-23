1d6d68666c0a4e9bb0146b90460487cfd7df91cd
COHL7500 <cohl7500@gmail.com>
2022-09-23 14:00:01 +0200

    (partially) implement KanbanContext.cs

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -1,5 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace Assignment3.Entities;
 
-public class KanbanContext
+public class KanbanContext : DbContext
 {
+    public virtual DbSet<User> Users => Set<User>();
+
+    public virtual DbSet<Tag> Tags => Set<Tag>();
+
+    public virtual DbSet<Task> Tasks => Set<Task>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .Property(u => u.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Task>()
+            .Property(t => t.Title)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Task>()
+            .Property(t => t.State)
+            .IsRequired();
+
+    }
+
 }
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,5 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace Assignment3.Entities;
 
 public class User
 {
+    public int Id { set; get; }
+    
+    public string Name { get; set; }
+    
+    public string Email { get; set; } = null!;
+
+    public IEnumerable<Task> Tasks { get; set; } = null!;
+
 }
