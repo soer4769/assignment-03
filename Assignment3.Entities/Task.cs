dd6f01805c7cd480718ec3b56424743168cba8b7
COHL7500 <cohl7500@gmail.com>
2022-09-23 13:04:25 +0200

    Implement Task

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -1,5 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
+
+public enum State {New, Active, Resolved, Closed, Removed}
 
 public class Task
 {
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    public required string Title {
+        get;
+        set;
+    }
+    
+    public required State state { get; set; }
+    
+    public User? AssignedTo { get; set; }
+    
+    public string? Description { get; set; }
+    
+    public IEnumerable<Tag> Tags { get; set; }
 }
