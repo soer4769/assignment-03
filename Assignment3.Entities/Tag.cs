3daf42753b97e8a3f3c88aa98aae166863e0e5a7
oljh <oljh@itu.dk>
2022-09-23 13:24:23 +0200

    Implemented Tag at POCO

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -2,4 +2,12 @@ namespace Assignment3.Entities;
 
 public class Tag
 {
+    public int Id {get; set;}
+
+    [Required]
+    [StringLength(50)]
+    public string Name;
+    
+    
+    public IEnumerable<Task> Tasks;
 }
